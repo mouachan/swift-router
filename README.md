ced068b85f24cf97ed77168e372b251dd67fa1ef
mouachan <mourad.ouachani@gmail.com>
2021-12-05 22:04:47 +0100

    dmn kogito quarkus, metrics, grafana, prometherus, event listeners

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # swift-router
+
+# generate the project on quarkus
+mvn archetype:generate \
+-DarchetypeGroupId=org.kie.kogito \
+-DarchetypeArtifactId=kogito-quarkus-dm-archetype \
+-DgroupId=com.redhat -DartifactId=swift-router-kogito-quarkus \
+-DarchetypeVersion=1.5.0.redhat-00006 \
+-Dversion=1.0-SNAPSHOT
+
+# generate the project on springboot
+mvn archetype:generate \
+-DarchetypeGroupId=org.kie.kogito \
+-DarchetypeArtifactId=kogito-springboot-dm-archetype \
+-DgroupId=org.redhat -DartifactId=swift-router-kogito-springboot \
+-DarchetypeVersion=1.5.0.redhat-00006 \
+-Dversion=1.0-SNAPSHOT
+
+# rule sample
+when 
+receiverAddress = 'BNPAFRPP...' and messageType/code = 'MT012’ and TRN = ‘Test…’ and document/data =
+'(.*[{]4:.*103[:]EBA.*[{]5:.*|.*[{]4:.*103[:]ERP.*[{]5:.*)’
+then 
+codeRoutage = CHG02
---
+++ swift-router-kogito-quarkus/src/main/java/com/redhat/listeners/LoggingDMNRuntimeEventListener.java
@@ -0,0 +1,72 @@
+package com.redhat.listeners;
+
+import org.kie.dmn.api.core.event.AfterEvaluateAllEvent;
+import org.kie.dmn.api.core.event.AfterEvaluateContextEntryEvent;
+import org.kie.dmn.api.core.event.AfterEvaluateDecisionEvent;
+import org.kie.dmn.api.core.event.AfterEvaluateDecisionTableEvent;
+import org.kie.dmn.api.core.event.BeforeEvaluateAllEvent;
+import org.kie.dmn.api.core.event.BeforeEvaluateContextEntryEvent;
+import org.kie.dmn.api.core.event.BeforeEvaluateDecisionEvent;
+import org.kie.dmn.api.core.event.BeforeEvaluateDecisionTableEvent;
+import org.kie.dmn.api.core.event.DMNRuntimeEventListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Simple utility class that logs the name of received events, used as base class of
+ * all the {@link DMNRuntimeEventListener}s instantiated in this example.
+ */
+class LoggingDMNRuntimeEventListener implements DMNRuntimeEventListener {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DMNRuntimeEventListener.class);
+    private final String name;
+
+    public LoggingDMNRuntimeEventListener(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void beforeEvaluateDecision(BeforeEvaluateDecisionEvent event) {
+        //log("BeforeEvaluateDecisionEvent");
+    }
+
+    @Override
+    public void afterEvaluateDecision(AfterEvaluateDecisionEvent event) {
+        //log("Decision name : "+event.getDecision().getName());
+    }
+
+    @Override
+    public void beforeEvaluateContextEntry(BeforeEvaluateContextEntryEvent event) {
+       // log("BeforeEvaluateContextEntryEvent");
+    }
+
+    @Override
+    public void afterEvaluateContextEntry(AfterEvaluateContextEntryEvent event) {
+       // log("AfterEvaluateContextEntryEvent");
+    }
+
+    @Override
+    public void beforeEvaluateDecisionTable(BeforeEvaluateDecisionTableEvent event) {
+       // log("BeforeEvaluateDecisionTableEvent");
+    }
+
+    @Override
+    public void afterEvaluateDecisionTable(AfterEvaluateDecisionTableEvent event) {
+        log("node name  "+event.getNodeName() + " table name "+event.getDecisionTableName()+" line "+event.getSelected().get(0));
+    }
+
+    @Override
+    public void beforeEvaluateAll(BeforeEvaluateAllEvent event) {
+        //log("BeforeEvaluateAllEvent");
+    }
+
+    @Override
+    public void afterEvaluateAll(AfterEvaluateAllEvent event) {
+        //log("AfterEvaluateAllEvent");
+    }
+
+    private void log(String event) {
+        LOG.info("{} received by {}", event, name);
+    }
+
+}
